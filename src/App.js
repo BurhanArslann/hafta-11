919a655d4f2f3c97696fb3588f3c6d71ab855fa5
BurhanArslann <arslann.burhan.git@gmail.com>
2024-03-24 17:56:27 +0300

    Global Context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
+import { useState } from "react";
+import C1 from "./bilesenler/C1";
+import GlobalContext from "./contexts/GlobalContext";
 
 function App() {
-  return (
-    <>
-    </>
-  )
+  const [kullanici, kullaniciGuncelle] = useState("Orhan Gencebay");
 
+  return (
+    <GlobalContext.Provider value={kullanici}>
+      <div>
+        <h1>App</h1>
+        <C1 />
+      </div>
+    </GlobalContext.Provider>
+  );
 }
 
 export default App;
